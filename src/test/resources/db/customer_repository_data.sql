b4e8a84b4fa1a7fe5a15821f4e30a07fb7f3f5fb
Yuriy Tsarenko <tsarenkoyuriidev@gmail.com>
2023-06-01 15:43:18 +0300

    added lesson examples

--- src/test/resources/db/customer_repository_data.sql
+++ src/test/resources/db/customer_repository_data.sql
@@ -1,6 +1,6 @@
 -- Create the customers table
 CREATE TABLE IF NOT EXISTS customers (
-    customer_id BIGINT PRIMARY KEY,
+    customer_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(50),
     contact_name VARCHAR(50),
     country VARCHAR(50)
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS customers (
 DELETE FROM customers;
 
 -- insert statements for Table 1: customers
-INSERT INTO customers (customer_id, customer_name, contact_name, country) VALUES
-(1, 'Customer 1', 'John Smith', 'USA'),
-(2, 'Customer 2', 'Jane Doe', 'Canada'),
-(3, 'Customer 3', 'Miguel Rodriguez', 'Mexico');
+INSERT INTO customers (customer_name, contact_name, country) VALUES
+('Customer 1', 'John Smith', 'USA'),
+('Customer 2', 'Jane Doe', 'Canada'),
+('Customer 3', 'Miguel Rodriguez', 'Mexico');
